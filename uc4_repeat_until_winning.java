4020d2ebad064595298edf58acacfaed0041d61a
ganeshalapati <alapatiganesh31@gmail.com>
2021-05-17 12:32:16 +0530

    check until winning

---
+++ uc4_repeat_until_winning.java
@@ -0,0 +1,40 @@
+
+public class uc4_repeat_until_winning {
+	public static final int NO_PLAY = 0 ;
+	public static final int LADDER = 1 ;
+	public static final int SNAKE = 2 ;
+	
+	public static void main(String[] args) {
+		System.out.println("Wellcome to the Snake and Ladder game.");
+		
+		int position=0;
+		int newPosition=0;
+		int winPosition=100;
+		
+		while (position<=winPosition) {
+		int dieRolls = (int) (Math.random()*6)+1;
+		int checkOption = (int) (Math.random()*3);
+		
+		switch (checkOption) {
+		case NO_PLAY : 
+			newPosition = 0;
+			break;
+		case LADDER :
+			newPosition = dieRolls ;
+			break;
+		case SNAKE :
+			newPosition = -dieRolls ;
+			break;
+		}
+		position=position+newPosition;
+		if (position<0) {
+			position = 0;
+		}
+		System.out.println("your are now at "+position+"th"+" position");
+		}
+		
+	} 
+}
+
+
+
