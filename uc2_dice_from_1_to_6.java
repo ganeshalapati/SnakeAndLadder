e77b1e4c21bf3998dce8ac6c29d153ba475d9c8b
ganeshalapati <alapatiganesh31@gmail.com>
2021-05-17 12:29:33 +0530

    number from dice

---
+++ uc2_dice_from_1_to_6.java
@@ -0,0 +1,16 @@
+
+public class uc2_dice_from_1_to_6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to the player");
+		
+		 int start_position = 0;
+		int dice = (int) ((Math.random() * 6) + 1);
+		
+				System.out.println("rolled number is " +dice);
+
+
+	}
+
+}
