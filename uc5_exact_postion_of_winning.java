c5a7a322d8d91418820f30c0908ff74ebbb9c032
ganeshalapati <alapatiganesh31@gmail.com>
2021-05-17 12:33:16 +0530

    exact winning position

---
+++ uc5_exact_postion_of_winning.java
@@ -0,0 +1,40 @@
+
+public class uc5_exact_postion_of_winning {
+	public static final int NO_PLAY = 0 ;
+	public static final int LADDER = 1 ;
+	public static final int SNAKE = 2 ;
+	
+	public static void main(String[] args) {
+		System.out.println("Wellcome to the Snake and Ladder game.");
+		
+		int position=0;
+		int newPosition=0;
+		int winPosition=100;
+		
+		while (position<winPosition) {
+		int dieRolls = (int) (Math.random()*6)+1;
+		int checkOption = (int) (Math.random()*3);
+		
+		switch (checkOption) {
+		case NO_PLAY : 
+			newPosition = 0;
+			break;
+		case LADDER :
+			newPosition = dieRolls ;
+			break;
+		case SNAKE :
+			newPosition = -dieRolls ;
+			break;
+		}
+		position=position+newPosition;
+		if (position<0) {
+			position = 0;
+		}
+		if (position>100) {
+			position=newPosition;
+		}
+		}
+		System.out.println("your are now at "+position+"th"+" position");
+	} 
+
+}
