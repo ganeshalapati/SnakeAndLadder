46dad49cb24c9568e5f91cd6b4b16a2424d4a3de
ganeshalapati <alapatiganesh31@gmail.com>
2021-05-17 12:27:28 +0530

    player introduction

---
+++ welcome_to_player.java
@@ -0,0 +1,12 @@
+
+public class welcome_to_player {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to the player");
+		
+		 int start_position = 0;
+
+	}
+
+}
